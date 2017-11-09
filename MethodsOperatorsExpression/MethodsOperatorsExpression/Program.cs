ca081e72bb86103dd10ceb1507ee8164e9299a2d
Woraprad <58030217@kmitl.ac.th>
2017-11-09 22:46:44 +0700

    ตัวดำเนินการกำหนดค่าโดยเครื่องหมาย  =

--- MethodsOperatorsExpression/MethodsOperatorsExpression/Program.cs
+++ MethodsOperatorsExpression/MethodsOperatorsExpression/Program.cs
@@ -4,23 +4,12 @@ class Program
 {
     static void Main()
     {
-        int x = 17;
-        double y = x / 4.0;
-        double z = y * y;
-        Console.WriteLine(y);
-        Console.WriteLine(z);
+        int year = 2017;
+        string helloWorld = "Hello World.";
+        int lastyear = year - 1;
 
-        int a = 5;
-        int b = 4;
-        Console.WriteLine(a + b);
-        Console.WriteLine(a + (b++));
-        Console.WriteLine(a + b);
-        Console.WriteLine(a + (++b));
-        Console.WriteLine(a + b);
-        Console.WriteLine(14 / a);
-        Console.WriteLine(14 % a);
 
     }
 
-    
+
 }
