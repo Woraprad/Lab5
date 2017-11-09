4afbd54c11a66730566c0e8d9e4124b328bf48a6
Woraprad <58030217@kmitl.ac.th>
2017-11-10 00:30:07 +0700

    1. เขียนโปรแกรม โดยรับอินพุตมาเป็นตัวเลข แล้วมีเมธอดหนึ่ง รับตัวเลขนั้นไปพิจารณา แล้วตอบกลับมาว่า เป็นเลขคู่หรือเลขคี่

--- MethodsOperatorsExpression/MethodsOperatorsExpression/Program.cs
+++ MethodsOperatorsExpression/MethodsOperatorsExpression/Program.cs
@@ -2,13 +2,32 @@
 
 class Program
 {
-    static void Main()
+        static void Main()
     {
-        int a = 2;
-        string myString = a.ToString();
-        Console.WriteLine(myString);
+        int a;
+        int b;
+        Console.Write("Input Number:");
+            a = Console.Read();
 
+        b = mod(a);
+        
+        if (b == 1)
+        {
+            Console.WriteLine("Odd number");
+        } 
+        else
+        {
+            Console.WriteLine("even number");
+        }
+        Console.ReadKey();
     }
 
+    static int mod(int a)
+    {
+        return a%2;
+    }
 
 }
+
+
+
